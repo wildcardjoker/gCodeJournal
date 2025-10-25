5978f4ffdf5e9e29503ee0f89656c1caf2dcb8bb
wildcardjoker <wildcardjoker@users.noreply.github.com>
2025-10-25 22:00:22 +0800

    ♻️ Refactor `GCodeJournalViewModel` and interface
    * 📝 Replaced XML comments with `<inheritdoc />` for clarity.
    * 🐛 Fixed swapped implementations of `AddCustomerAsync` and `AddFilamentAsync`.
    * 📝 Updated `IGCodeJournalViewModel` with detailed method summaries.
    * ♻️ Refactored `GCodeJournalViewModel` to align with the interface.
    * 🎨 Reordered methods for logical grouping and readability.
    * 🎨 Reformatted `GetAllPrintingProjectsAsync` for better readability.
    * 🔥 Removed unnecessary `using` directives for cleaner code.
    * 📝 Improved interface documentation for better developer usability.

--- src/gCodeJournal.ViewModel/GCodeJournalViewModel.cs
+++ src/gCodeJournal.ViewModel/GCodeJournalViewModel.cs
@@ -3,16 +3,9 @@
 #region Using Directives
 using Microsoft.EntityFrameworkCore;
 using Model;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 #endregion
 
 /// <inheritdoc />
-/// <summary>
-///     Represents the ViewModel for the GCodeJournal application. This class extends the
-///     <see cref="T:gCodeJournal.Model.GCodeJournalDbContext">GCodeJournalDbContext</see>
-///     to provide additional functionality for managing and querying database entities related to the application.
-/// </summary>
 public class GCodeJournalViewModel : IGCodeJournalViewModel
 {
     #region Fields
@@ -35,61 +28,6 @@ public class GCodeJournalViewModel : IGCodeJournalViewModel
 
     #region IGCodeJournalViewModel Members
     /// <inheritdoc />
-    public async Task AddFilamentAsync(Filament filament)
-    {
-        ArgumentNullException.ThrowIfNull(filament);
-        await _db.Filaments.AddAsync(filament).ConfigureAwait(false);
-        await _db.SaveChangesAsync().ConfigureAwait(false);
-    }
-
-    /// <summary>
-    ///     Retrieves a list of customers from the database, ordered by their names.
-    /// </summary>
-    /// <returns>
-    ///     A task that represents the asynchronous operation. The task result contains a list of
-    ///     <see cref="T:gCodeJournal.Model.Customer">Customer</see> entities, ordered by their names.
-    /// </returns>
-    /// <remarks>
-    ///     This method queries the <see cref="P:gCodeJournal.Model.GCodeJournalDbContext.Customers">Customers</see> DbSet
-    ///     and orders the results by the <see cref="P:gCodeJournal.Model.Customer.Name">Name</see> property.
-    /// </remarks>
-    public Task<List<Customer>> GetAllCustomersAsync() => _db.Customers.OrderBy(c => c.Name).ToListAsync();
-
-    /// <summary>
-    ///     Retrieves a list of filaments from the database, ordered by manufacturer and color.
-    /// </summary>
-    /// <returns>
-    ///     A task representing the asynchronous operation. The task result contains a list of <see cref="Filament" />
-    ///     entities.
-    /// </returns>
-    public Task<List<Filament>> GetAllFilamentsAsync() => _db.Filaments.OrderBy(f => f.Manufacturer).ThenBy(c => c.Colour).ToListAsync();
-
-    /// <summary>
-    ///     Retrieves a list of manufacturers from the database, ordered by name.
-    /// </summary>
-    /// <returns>
-    ///     A task representing the asynchronous operation. The task result contains a list of <see cref="Manufacturer" />
-    ///     entities.
-    /// </returns>
-    public Task<List<Manufacturer>> GetAllManufacturersAsync() => _db.Manufacturers.OrderBy(m => m.Name).ToListAsync();
-
-    /// <summary>
-    ///     Asynchronously adds a new customer to the database.
-    /// </summary>
-    /// <param name="customer">
-    ///     The <see cref="T:gCodeJournal.Model.Customer">Customer</see> instance to add.
-    ///     This parameter must not be <see langword="null" />.
-    /// </param>
-    /// <exception cref="T:System.ArgumentNullException">
-    ///     Thrown when the <paramref name="customer" /> parameter is <see langword="null" />.
-    /// </exception>
-    /// <remarks>
-    ///     This method adds the specified customer to the <see cref="P:gCodeJournal.Model.GCodeJournalDbContext.Customers" />
-    ///     collection and persists the changes to the database.
-    /// </remarks>
-    /// <returns>
-    ///     A <see cref="T:System.Threading.Tasks.Task" /> that represents the asynchronous operation.
-    /// </returns>
     public async Task AddCustomerAsync(Customer customer)
     {
         ArgumentNullException.ThrowIfNull(customer);
@@ -97,17 +35,15 @@ public class GCodeJournalViewModel : IGCodeJournalViewModel
         await _db.SaveChangesAsync().ConfigureAwait(false);
     }
 
-    /// <summary>
-    ///     Adds a new filament colour to the database.
-    /// </summary>
-    /// <param name="filamentColour">The filament colour entity to be added.</param>
-    /// <exception cref="ArgumentNullException">
-    ///     Thrown if the <paramref name="filamentColour" /> is <c>null</c>.
-    /// </exception>
-    /// <remarks>
-    ///     This method ensures that the provided <see cref="FilamentColour" /> entity is added to the
-    ///     <see cref="GCodeJournalDbContext.FilamentColours" /> collection and persists the changes to the database.
-    /// </remarks>
+    /// <inheritdoc />
+    public async Task AddFilamentAsync(Filament filament)
+    {
+        ArgumentNullException.ThrowIfNull(filament);
+        await _db.Filaments.AddAsync(filament).ConfigureAwait(false);
+        await _db.SaveChangesAsync().ConfigureAwait(false);
+    }
+
+    /// <inheritdoc />
     public async Task AddFilamentColourAsync(FilamentColour filamentColour)
     {
         ArgumentNullException.ThrowIfNull(filamentColour);
@@ -115,16 +51,7 @@ public class GCodeJournalViewModel : IGCodeJournalViewModel
         await _db.SaveChangesAsync().ConfigureAwait(false);
     }
 
-    /// <summary>
-    ///     Adds a new filament type to the database.
-    /// </summary>
-    /// <param name="filamentType">The filament type entity to be added.</param>
-    /// <exception cref="ArgumentNullException">Thrown if the <paramref name="filamentType" /> is null.</exception>
-    /// <remarks>
-    ///     This method ensures that the provided <see cref="T:gCodeJournal.Model.FilamentType" />
-    ///     is added to the <see cref="P:gCodeJournal.Model.GCodeJournalDbContext.FilamentTypes" /> collection
-    ///     and persists the changes to the database.
-    /// </remarks>
+    /// <inheritdoc />
     public async Task AddFilamentTypeAsync(FilamentType filamentType)
     {
         ArgumentNullException.ThrowIfNull(filamentType);
@@ -132,46 +59,15 @@ public class GCodeJournalViewModel : IGCodeJournalViewModel
         await _db.SaveChangesAsync().ConfigureAwait(false);
     }
 
-    /// <summary>
-    ///     Adds a new <see cref="T:gCodeJournal.Model.ModelDesign">ModelDesign</see> entity to the database.
-    /// </summary>
-    /// <param name="filamentColour">
-    ///     The <see cref="T:gCodeJournal.Model.ModelDesign">ModelDesign</see> instance to be added.
-    /// </param>
-    /// <exception cref="T:System.ArgumentNullException">
-    ///     Thrown when the <paramref name="filamentColour" /> parameter is <see langword="null" />.
-    /// </exception>
-    /// <remarks>
-    ///     This method asynchronously adds the provided <see cref="T:gCodeJournal.Model.ModelDesign">ModelDesign</see>
-    ///     to the <see cref="P:gCodeJournal.Model.GCodeJournalDbContext.ModelDesigns">ModelDesigns</see> DbSet
-    ///     and commits the changes to the database.
-    /// </remarks>
-    /// <returns>
-    ///     A <see cref="T:System.Threading.Tasks.Task">Task</see> that represents the asynchronous operation.
-    /// </returns>
-    public async Task AddModelDesignAsync(ModelDesign filamentColour)
+    /// <inheritdoc />
+    public async Task AddModelDesignAsync(ModelDesign modelDesign)
     {
-        ArgumentNullException.ThrowIfNull(filamentColour);
-        await _db.ModelDesigns.AddAsync(filamentColour).ConfigureAwait(false);
+        ArgumentNullException.ThrowIfNull(modelDesign);
+        await _db.ModelDesigns.AddAsync(modelDesign).ConfigureAwait(false);
         await _db.SaveChangesAsync().ConfigureAwait(false);
     }
 
-    /// <summary>
-    ///     Asynchronously adds a new printing project to the database.
-    /// </summary>
-    /// <param name="project">
-    ///     The <see cref="T:gCodeJournal.Model.PrintingProject" /> instance representing the printing project to be added.
-    /// </param>
-    /// <exception cref="ArgumentNullException">
-    ///     Thrown when the <paramref name="project" /> parameter is <c>null</c>.
-    /// </exception>
-    /// <remarks>
-    ///     This method adds the specified <see cref="T:gCodeJournal.Model.PrintingProject" /> to the database context and
-    ///     saves the changes.
-    /// </remarks>
-    /// <returns>
-    ///     A <see cref="T:System.Threading.Tasks.Task" /> representing the asynchronous operation.
-    /// </returns>
+    /// <inheritdoc />
     public async Task AddPrintingProjectAsync(PrintingProject project)
     {
         ArgumentNullException.ThrowIfNull(project);
@@ -179,62 +75,28 @@ public class GCodeJournalViewModel : IGCodeJournalViewModel
         await _db.SaveChangesAsync().ConfigureAwait(false);
     }
 
-    /// <summary>
-    ///     Retrieves a list of filament colours from the database, ordered by their description.
-    /// </summary>
-    /// <returns>
-    ///     A task that represents the asynchronous operation. The task result contains a list of
-    ///     <see cref="T:gCodeJournal.Model.FilamentColour">FilamentColour</see> objects, ordered by their description.
-    /// </returns>
-    /// <remarks>
-    ///     This method queries the
-    ///     <see cref="P:gCodeJournal.Model.GCodeJournalDbContext.FilamentColours">FilamentColours</see>
-    ///     DbSet and returns the results as a list. The results are sorted alphabetically by the
-    ///     <see cref="P:gCodeJournal.Model.FilamentColour.Description">Description</see>.
-    /// </remarks>
+    /// <inheritdoc />
+    public Task<List<Customer>> GetAllCustomersAsync() => _db.Customers.OrderBy(c => c.Name).ToListAsync();
+
+    /// <inheritdoc />
     public Task<List<FilamentColour>> GetAllFilamentColoursAsync() => _db.FilamentColours.OrderBy(fc => fc.Description).ToListAsync();
 
-    /// <summary>
-    ///     Retrieves a list of all available filament types from the database, ordered by their description.
-    /// </summary>
-    /// <returns>
-    ///     A task that represents the asynchronous operation. The task result contains a list of
-    ///     <see cref="T:gCodeJournal.Model.FilamentType">FilamentType</see> objects.
-    /// </returns>
-    /// <remarks>
-    ///     This method queries the <see cref="P:gCodeJournal.Model.GCodeJournalDbContext.FilamentTypes">FilamentTypes</see>
-    ///     DbSet and orders the results by the <see cref="P:gCodeJournal.Model.FilamentType.Description">Description</see>
-    ///     property.
-    /// </remarks>
+    /// <inheritdoc />
+    public Task<List<Filament>> GetAllFilamentsAsync() => _db.Filaments.OrderBy(f => f.Manufacturer).ThenBy(c => c.Colour).ToListAsync();
+
+    /// <inheritdoc />
     public Task<List<FilamentType>> GetAllFilamentTypesAsync() => _db.FilamentTypes.OrderBy(ft => ft.Description).ToListAsync();
 
-    /// <summary>
-    ///     Retrieves a list of <see cref="T:gCodeJournal.Model.ModelDesign">ModelDesign</see> entities
-    ///     ordered by their description.
-    /// </summary>
-    /// <returns>
-    ///     A task that represents the asynchronous operation. The task result contains a list of
-    ///     <see cref="T:gCodeJournal.Model.ModelDesign">ModelDesign</see> objects.
-    /// </returns>
-    /// <remarks>
-    ///     This method queries the <see cref="P:gCodeJournal.Model.GCodeJournalDbContext.ModelDesigns">ModelDesigns</see>
-    ///     DbSet, orders the results by the <see cref="P:gCodeJournal.Model.ModelDesign.Description">Description</see>
-    ///     property, and returns the results as a list.
-    /// </remarks>
+    /// <inheritdoc />
+    public Task<List<Manufacturer>> GetAllManufacturersAsync() => _db.Manufacturers.OrderBy(m => m.Name).ToListAsync();
+
+    /// <inheritdoc />
     public Task<List<ModelDesign>> GetAllModelDesignsAsync() => _db.ModelDesigns.OrderBy(md => md.Description).ToListAsync();
 
-    /// <summary>
-    ///     Retrieves a list of <see cref="PrintingProject" /> entities from the database.
-    /// </summary>
-    /// <remarks>
-    ///     The resulting list is ordered by the description of the associated <see cref="ModelDesign" />,
-    ///     followed by the <see cref="Customer" /> who requested the project, and then by the associated
-    ///     <see cref="Filament" /> entities in ascending order.
-    /// </remarks>
-    /// <returns>
-    ///     A <see cref="Task{TResult}" /> representing the asynchronous operation. The task result contains
-    ///     a list of <see cref="PrintingProject" /> entities.
-    /// </returns>
-    public Task<List<PrintingProject>> GetAllPrintingProjectsAsync() => _db.PrintingProjects.OrderBy(pp => pp.Model.Description).ThenBy(c => c.Customer).ThenBy(f => f.Filaments.OrderBy(f1 => f1.ToString())).ToListAsync();
+    /// <inheritdoc />
+    public Task<List<PrintingProject>> GetAllPrintingProjectsAsync() => _db.PrintingProjects.OrderBy(pp => pp.Model.Description)
+                                                                           .ThenBy(c => c.Customer)
+                                                                           .ThenBy(f => f.Filaments.OrderBy(f1 => f1))
+                                                                           .ToListAsync();
     #endregion
 }
--- src/gCodeJournal.ViewModel/IGCodeJournalViewModel.cs
+++ src/gCodeJournal.ViewModel/IGCodeJournalViewModel.cs
@@ -1,18 +1,98 @@
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using gCodeJournal.Model;
-
 namespace gCodeJournal.ViewModel
 {
- public interface IGCodeJournalViewModel
- {
- Task AddFilamentAsync(Filament filament);
- Task<List<Filament>> GetAllFilamentsAsync();
- Task<List<Manufacturer>> GetAllManufacturersAsync();
- Task<List<Customer>> GetAllCustomersAsync();
- Task<List<FilamentColour>> GetAllFilamentColoursAsync();
- Task<List<FilamentType>> GetAllFilamentTypesAsync();
- Task<List<ModelDesign>> GetAllModelDesignsAsync();
- Task<List<PrintingProject>> GetAllPrintingProjectsAsync();
- }
+    #region Using Directives
+    using Model;
+    #endregion
+
+    /// <summary>
+    ///     Represents the view model interface for managing GCode Journal operations.
+    ///     Provides methods to add and retrieve data related to customers, filaments, filament colors, filament types, model
+    ///     designs, and printing projects.
+    /// </summary>
+    public interface IGCodeJournalViewModel
+    {
+        /// <summary>
+        ///     Adds a new customer asynchronously.
+        /// </summary>
+        /// <param name="customer">The customer to add.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        Task AddCustomerAsync(Customer customer);
+
+        /// <summary>
+        ///     Adds a new filament asynchronously.
+        /// </summary>
+        /// <param name="filament">The filament to add.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        Task AddFilamentAsync(Filament filament);
+
+        /// <summary>
+        ///     Adds a new filament color asynchronously.
+        /// </summary>
+        /// <param name="filamentColour">The filament color to add.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        Task AddFilamentColourAsync(FilamentColour filamentColour);
+
+        /// <summary>
+        ///     Adds a new filament type asynchronously.
+        /// </summary>
+        /// <param name="filamentType">The filament type to add.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        Task AddFilamentTypeAsync(FilamentType filamentType);
+
+        /// <summary>
+        ///     Adds a new model design asynchronously.
+        /// </summary>
+        /// <param name="modelDesign">The model design to add.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        Task AddModelDesignAsync(ModelDesign modelDesign);
+
+        /// <summary>
+        ///     Adds a new printing project asynchronously.
+        /// </summary>
+        /// <param name="project">The printing project to add.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        Task AddPrintingProjectAsync(PrintingProject project);
+
+        /// <summary>
+        ///     Retrieves all customers asynchronously.
+        /// </summary>
+        /// <returns>A task representing the asynchronous operation, with a list of customers as the result.</returns>
+        Task<List<Customer>> GetAllCustomersAsync();
+
+        /// <summary>
+        ///     Retrieves all filament colors asynchronously.
+        /// </summary>
+        /// <returns>A task representing the asynchronous operation, with a list of filament colors as the result.</returns>
+        Task<List<FilamentColour>> GetAllFilamentColoursAsync();
+
+        /// <summary>
+        ///     Retrieves all filaments asynchronously.
+        /// </summary>
+        /// <returns>A task representing the asynchronous operation, with a list of filaments as the result.</returns>
+        Task<List<Filament>> GetAllFilamentsAsync();
+
+        /// <summary>
+        ///     Retrieves all filament types asynchronously.
+        /// </summary>
+        /// <returns>A task representing the asynchronous operation, with a list of filament types as the result.</returns>
+        Task<List<FilamentType>> GetAllFilamentTypesAsync();
+
+        /// <summary>
+        ///     Retrieves all manufacturers asynchronously.
+        /// </summary>
+        /// <returns>A task representing the asynchronous operation, with a list of manufacturers as the result.</returns>
+        Task<List<Manufacturer>> GetAllManufacturersAsync();
+
+        /// <summary>
+        ///     Retrieves all model designs asynchronously.
+        /// </summary>
+        /// <returns>A task representing the asynchronous operation, with a list of model designs as the result.</returns>
+        Task<List<ModelDesign>> GetAllModelDesignsAsync();
+
+        /// <summary>
+        ///     Retrieves all printing projects asynchronously.
+        /// </summary>
+        /// <returns>A task representing the asynchronous operation, with a list of printing projects as the result.</returns>
+        Task<List<PrintingProject>> GetAllPrintingProjectsAsync();
+    }
 }
