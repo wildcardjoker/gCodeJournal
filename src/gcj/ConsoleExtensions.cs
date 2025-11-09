754c93b3610ad152363e4168ed5d17190205da17
wildcardjoker <wildcardjoker@users.noreply.github.com>
2025-11-09 11:06:54 +0800

    ✨ Add async entity selection method to ConsoleExtensions
    🐛 Suppressed nullable warning `CS8714` for generic type `T`.
    🧵 Added `GetEntitySelectionAsync<T>` for async entity selection.
    ✨ Used `SelectionPrompt<T>` to display a customizable menu.
    💡 Included "Back to menu" option for `null` handling.
    🚨 Suppressed ReSharper warning for nullability condition.

--- src/gcj/ConsoleExtensions.cs
+++ src/gcj/ConsoleExtensions.cs
@@ -17,5 +17,21 @@
         }
 
         public static void DisplayInfoMessage(this string message) => message.DisplayConsoleMessageWithLeadingEmoji(Emoji.Known.Information);
+
+        public static async Task<T?> GetEntitySelectionAsync<T>(this IEnumerable<T> choices) where T : class
+        {
+#pragma warning disable CS8714 // The type cannot be used as type parameter in the generic type or method. Nullability of type argument doesn't match 'notnull' constraint.
+
+            // ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
+            var prompt = new SelectionPrompt<T>().Title($"Please select from the following {typeof(T).Name}")
+                                                 .PageSize(10)
+                                                 .MoreChoicesText("Scroll up/down for more choices")
+                                                 .UseConverter(item => item is null ? "Back to menu" : item?.ToString() ?? "<unknown>")!.AddChoices(
+                                                     new[] {(T?) null}.Concat(choices.Select(T? (c) => c)));
+#pragma warning restore CS8714 // The type cannot be used as type parameter in the generic type or method. Nullability of type argument doesn't match 'notnull' constraint.
+
+            var response = await AnsiConsole.PromptAsync(prompt).ConfigureAwait(false);
+            return response;
+        }
     }
 }
