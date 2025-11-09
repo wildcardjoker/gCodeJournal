080c1716d4511bbe15e17425c340a8353edcc854
wildcardjoker <wildcardjoker@users.noreply.github.com>
2025-11-09 19:54:08 +0800

    ✨ Add `ValidateCustomerName` method for input validation
    - ✨ Introduced a new namespace `gcj` to organize code.
    - 🎨 Added `#region Using Directives` for `Microsoft.Extensions.Logging` and `Spectre.Console`.
    - ✨ Created a static partial class `Program` to encapsulate logic.
    - 🧵 Implemented `ValidateCustomerName` async method to validate user input.
    - 🔊 Logged an error with `ILogger` if the customer name is empty.
    - ✨ Enhanced user interaction with `Spectre.Console` for console input.

---
+++ src/gcj/Actions/Validation.cs
@@ -0,0 +1,21 @@
+﻿namespace gcj
+{
+    #region Using Directives
+    using Microsoft.Extensions.Logging;
+    using Spectre.Console;
+    #endregion
+
+    public static partial class Program
+    {
+        private static async Task<string?> ValidateCustomerName(ILogger appLogger)
+        {
+            var customerName = await "customer's name".GetInputFromConsoleAsync().ConfigureAwait(false);
+            if (string.IsNullOrWhiteSpace(customerName))
+            {
+                appLogger.LogError(Emoji.Known.Warning + "  Customer name cannot be empty");
+            }
+
+            return customerName;
+        }
+    }
+}
