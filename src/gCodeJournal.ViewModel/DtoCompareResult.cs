8446a08cb4cc969677fd54dd8f22bf863248e328
wildcardjoker <wildcardjoker@users.noreply.github.com>
2026-01-24 19:11:12 +0800

    Add DTO comparison result record and FilamentDto comparer
    Introduced DtoCompareResult record to encapsulate DTO comparison results, including match status and mismatched properties. Added an extension method CompareTo for FilamentDto to compare instances and return detailed differences.

---
+++ src/gCodeJournal.ViewModel/DtoCompareResult.cs
@@ -0,0 +1,12 @@
+﻿namespace gCodeJournal.ViewModel;
+
+/// <summary>
+/// Represents the result of comparing two DTOs (Data Transfer Objects).
+/// </summary>
+/// <param name="IsMatch">
+/// Indicates whether the compared DTOs are identical.
+/// </param>
+/// <param name="MismatchedProperties">
+/// A list of property names that differ between the compared DTOs.
+/// </param>
+public record DtoCompareResult(bool IsMatch, List<string> MismatchedProperties);
---
+++ src/gCodeJournal.ViewModel/ExtensionMethods.cs
@@ -0,0 +1,27 @@
+﻿using gCodeJournal.ViewModel.DTOs;
+
+namespace gCodeJournal.ViewModel;
+
+public static class ExtensionMethods
+{
+    public static DtoCompareResult CompareTo(this FilamentDto filamentDto, FilamentDto other)
+    {
+        var errors = new List<string>();
+
+        if (filamentDto.Id != other.Id)
+            errors.Add(nameof(filamentDto.Id));
+        if (filamentDto.ProductId != other.ProductId)
+            errors.Add(nameof(filamentDto.ProductId));
+        if (filamentDto.Manufacturer.Id != other.Manufacturer.Id)
+            errors.Add("ManufacturerId");
+        if (filamentDto.CostPerWeight != other.CostPerWeight)
+            errors.Add(nameof(filamentDto.CostPerWeight));
+        if (filamentDto.FilamentColour.Id != other.FilamentColour.Id)
+            errors.Add("FilamentColourId");
+        if (filamentDto.FilamentType.Id != other.FilamentType.Id)
+            errors.Add("FilamentTypeId");
+        if (filamentDto.ReorderLink != other.ReorderLink)
+            errors.Add(nameof(filamentDto.ReorderLink));
+        return new DtoCompareResult(errors.Count == 0, errors);
+    }
+}
